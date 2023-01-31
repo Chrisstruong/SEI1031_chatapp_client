50e37fcbd8ebdca659b19720365a4612f6fc7be1
chrisstruong <chrisstruong@gmail.com>
2023-01-31 13:59:34 -0500

    Work on message

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,14 +1,17 @@
 import './message.css'
+import {format} from "timeago.js"
 
-function Message({own}) {
+function Message({ message, own }) {
     return (
-        <div className={own? "message own": "message"}>
+        <div className={own ? "message own" : "message"}>
             <div className="messageTop">
                 <img className='messageImg' src="https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="" />
-                <p className='messageText'>Hello this is a message</p>
+                <p className='messageText'>
+                    {message.text}
+                </p>
             </div>
             <div className="messageBottom">
-                1 hour ago
+                {message.createdAt}
 
             </div>
         </div>
--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -10,12 +10,13 @@ import userEvent from "@testing-library/user-event";
 
 function Messenger() {
     const[conversations, setConversations] = useState([])
+    const[currentChat, setCurrentChat] = useState(null)
+    const[messages, setMessages] = useState(null)
     const BASE_URL = `http://localhost:4000/conversations/63d93777d6b390a57e393319`
     const getUserConversation = async() =>{
         try{
             const response = await fetch(BASE_URL)
             const foundUserConversation = await response.json()
-            console.log(`foundUserConversation is ${foundUserConversation}`)
             setConversations(foundUserConversation)
         } catch (err){
             console.log(err)
@@ -24,44 +25,50 @@ function Messenger() {
     useEffect(()=>{
         getUserConversation()
     },[])
+    
+
+    const getMessages = async() => {
+        try {
+            const response = await fetch('http://localhost:4000/messages/'+currentChat?._id)
+            const foundMessage = await response.json()
+            setMessages(foundMessage)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+    useEffect(()=> {
+        getMessages()
+    }, [currentChat])
+
+    console.log(messages)
     return (
         <div className="messenger">
             <div className="chatMenu">
                 <div className="chatMenuWrapper">
                     <input placeholder = "Search for friends" className="chatMenuInput"/>
                     {conversations.map((c)=> (
+                        <div onClick={()=> setCurrentChat(c)}>
                         <Conversation conversation={c} currentUserId={"63d93777d6b390a57e393319"}/>
+                        </div>
                     ))}
                 </div>
 
             </div>
             <div className="chatBox">
                 <div className="chatBoxWrapper">
+                    {
+                        currentChat ? 
+                    <>
                     <div className="chatBoxTop">
-                        <Message own={true}/>
-                        <Message own={false}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
-                        <Message own={true}/>
+                        {messages.map((msg)=> (
+                            <Message message={msg} own={msg.sender === "63d93777d6b390a57e393319" }/>
+                        ))}
+                        
                     </div>
                     <div className="chatBoxBottom">
                         <textarea className="chatMessageInput" placeholder="write something..." ></textarea>
                         <button className="chatSubmitButton">Send</button>
-                    </div>
+                    </div></> : <span className="noConversationText">Open a conversation to start</span>}
                 </div>
 
             </div>
