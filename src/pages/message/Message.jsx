44b7bc35f90970ad66bc19cc85aa9344adc550a4
chrisstruong <chrisstruong@gmail.com>
2023-02-01 16:30:11 -0500

    Finish update and delete route in React for message

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -9,7 +9,6 @@ function Message({ message, own }) {
     const navigate = useNavigate()
     return (
         <div className={own ? "message own" : "message"}>
-            <h1></h1>
             <div className="messageTop">
                 <img className='messageImg' src="https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="" />
                 <div className='messageText' onMouseEnter={()=>setIsShown(true)} onMouseLeave={()=>setIsShown(false)}>
--- src/pages/message/MessageDetail.jsx
+++ src/pages/message/MessageDetail.jsx
@@ -1,8 +1,91 @@
+import {useState, useEffect} from 'react'
+import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 function MessageDetail (){
+    const [message, setMessage] = useState([])
+    const [newText, setNewText] = useState("")
+    const params = useParams()
+    const {id} = params
+    const navigate = useNavigate()
+
+    const getMessage = async ()=>{
+        try{
+            const response = await fetch(`http://localhost:4000/messages/detail/${id}`)
+            const foundMessage = await response.json()
+            console.log(foundMessage._id)
+            setMessage(foundMessage)
+        } catch (err){
+            console.log(err)
+        }
+    }
+
+    useEffect(()=> {
+        getMessage()
+    },[])
+
+    const removedMessage = async() => {
+        try {
+            const options = {
+                method:"DELETE",
+            }
+            const response = await fetch(`http://localhost:4000/messages/detail/${id}`, options)
+            const deletedMessage = await response.json
+            navigate(-1)
+        } catch(err){
+            console.log(err)
+        }
+    }
+
+
+    const updatedMessage = async (e) => {
+        e.preventDefault()
+        const newMessage = {
+            _id: message._id,
+            conversationId: message.conversationId,
+            sender: message.sender,
+            text: newText,
+        }
+        console.log(newMessage.text)
+        try {
+            const requestOptions = {
+                method:"PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newMessage)
+            }
+            const response = await fetch(`http://localhost:4000/messages/detail/${id}`, requestOptions)
+            const updatedMessage2 = await response.json()
+            setMessage(updatedMessage2)
+            navigate(-1)
+        } catch (err){
+            console.log(err)
+        }
+    }
+
+
+
     return(
         <>
+        <h1>Welcome to Edit/Delete Message page</h1>
+        <div>
+            <form onSubmit={updatedMessage}>
+                <label> Edit Message:
+                    <input
+                        type="text"
+                        value={newText}
+                        onChange={(e)=>setNewText(e.target.value)}
+                    />
+                </label>
+                <input type="submit" value="Edit message"/>
+            </form>
+        </div>
+        <div>
+            <button onClick={removedMessage}>Delete message</button>
+        </div>
         </>
     )
+    
 
 }
 
